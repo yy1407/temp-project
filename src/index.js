3b3fe6410c28e56bc63a65223bc2df54bea70e5e
yy1407 <yuuma20020715@icloud.com>
2025-08-12 17:51:44 +0900

    テキストファイルに出力するように変更

--- src/index.js
+++ src/index.js
@@ -2,17 +2,12 @@ const path = require('path');
 const _ = require('lodash');
 
 const { readPhpFilesSync } = require('./modules/fileReader');
-const {
-  extractFunctionsWithRelations,
-} = require('./modules/functionExtractor');
-const {
-  generateRelationDescription,
-} = require('./modules/relationDescriptions');
+const { extractFunctionsWithRelations } = require('./modules/functionExtractor');
+const { generateRelationDescription } = require('./modules/relationDescriptions');
 const { writeOutput } = require('./modules/outputWriter');
+const { pluralizeSnakeCase } = require('./modules/snakeCasePluralizer');
 
-const modelsDir = path.join(
-  '//wsl.localhost/Ubuntu/home/yyokota/api-web/app/Models',
-);
+const modelsDir = path.join('//wsl.localhost/Ubuntu/home/yyokota/api-web/app/Models');
 
 const phpFiles = readPhpFilesSync(modelsDir);
 
@@ -20,18 +15,15 @@ let output = '';
 
 phpFiles.forEach(({ filename, content }) => {
   const functionInfos = extractFunctionsWithRelations(content);
-  const snakeCaseFilename = _.snakeCase(filename.replace(/\.php$/i, ''));
+  const snakeCaseFilename = pluralizeSnakeCase(filename);
 
   output += `==== ${filename} ====\n`;
 
   functionInfos.forEach((info) => {
-    const snakeCaseFunctionName = _.snakeCase(info.functionName);
+    const snakeCaseFunctionName = pluralizeSnakeCase(info.functionName);
     output +=
-      generateRelationDescription(
-        info.relationType,
-        snakeCaseFilename,
-        snakeCaseFunctionName,
-      ) + '\n';
+      generateRelationDescription(info.relationType, snakeCaseFilename, snakeCaseFunctionName) +
+      '\n';
   });
 
   output += '\n';
---
+++ src/modules/snakeCasePluralizer.js
@@ -0,0 +1,19 @@
+const _ = require('lodash');
+const pluralize = require('pluralize');
+
+/**
+ * パスカルケースのファイル名や関数名（例: 'MCustomer.php'）から
+ * 拡張子を取り除き、スネークケースに変換し、
+ * 最後の単語だけ複数形に変換する関数 * @param {string} word パスカルケースのファイル名や関数名（例: 'MCustomer.php'）
+ * @returns {string} 拡張子を取り除き、複数形に変換されたスネークケース文字列（例: 'm_customers'）
+ */
+function pluralizeSnakeCase(word) {
+  const snakeCaseWord = _.snakeCase(word.replace(/\.php$/i, ''));
+  const parts = snakeCaseWord.split('_');
+  const last = parts.pop();
+  const pluralLast = pluralize(last);
+  parts.push(pluralLast);
+  return parts.join('_');
+}
+
+module.exports = { pluralizeSnakeCase };
